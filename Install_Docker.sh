6e3d4ae293d51c9eccc72cefeb9eb86c6fd41e83
SidGrip <32822318+SidGrip@users.noreply.github.com>
2021-12-12 14:01:38 -0600

    Docker Compose Install Script

---
+++ Install_Docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+DOCKER_COMPOSE_VERSION="1.26.2"
+
+# Install Docker
+    sudo apt-get update -y
+    sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 
+    sudo apt-key fingerprint 0EBFCD88
+    sudo add-apt-repository -y \
+        "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+        $(lsb_release -cs) \
+        stable"
+    sudo apt-get -y update
+    sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+
+    sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
