28e1d250dc40f31b2b3ca968a09fb12271eddcab
SidGrip <32822318+SidGrip@users.noreply.github.com>
2021-12-12 19:45:22 -0600

    Update Install_Docker.sh

--- Install_Docker.sh
+++ Install_Docker.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-# Install Docker
 apt-get update -y
 apt-get install -y docker-compose
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
